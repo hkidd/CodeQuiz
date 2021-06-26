7af4804ed596f5aaaea46df808c974a65cb4b033
hkidd <harrisonakidd@gmail.com>
2021-06-26 13:00:51 -0500

    Quiz is generally functioning correctly.  Answers are registered as correct or incorrect.  Need to fix timer and save results to local storage.

--- assets/script.js
+++ assets/script.js
@@ -10,9 +10,9 @@
 // WHEN I answer a question
 // THEN I am presented with another question
 //          1.  Need an array of questions and answers (array of objects?)
-//          2.  After clicking answer button, switch to another question/answer set
-//          3.  Questions will be randomly picked from the array and presented
-//          4.  Append list of answers to the question location inside body
+//          2.  After clicking answer button, switch to another question/answer set (increment question index)
+//          3.  Questions will be randomly picked from the array and presented (questions are randomized when startQuiz is called)
+//          4.  Append list of answers to the question location inside body (innerHTML will be used to show the questions and answers)
 //          5.  The random question selected will take the place replace the textContent of 
 
 
@@ -40,9 +40,11 @@ var questionText = document.querySelector('#question');
 var answerTextA = document.querySelector('#answer1');
 var answerTextB = document.querySelector('#answer2');
 var answerTextC = document.querySelector('#answer3');
-var answerTextAD = document.querySelector('#answer4');
+var answerTextD = document.querySelector('#answer4');
 var correctCount = document.querySelector('#correctAnswerCount');
 var resultsContainer = document.querySelector('#results');
+var totalCorrect = document.querySelector('#totalCorrect');
+var totalIncorrect = document.querySelector('#totalIncorrect');
 
 // Global variables
 var timeLeft = 30;
@@ -50,23 +52,22 @@ var correctAnswers = 0;
 var wrongAnswers = 0;
 var totalScore = 0;
 
-var questionIndex = 0;
-
 // Array of all questions.  From here, the individual properties can be accessed.
 var questions = [
     {
-        question: "What is the name of the F1 track in Austin?", answerA: "America's Track", 
+        question: "What is the name of the F1 track in Austin?", 
+        answerA: "America's Track", 
         answerB: "Austin Raceway", 
         answerC: "Circuit of the America's", 
         answerD: "Texas Motor Speedway",
-        answer: "C"
+        correct: "C"
     }, {
         question: "As of 2021, how many driver's championsips has Lewis Hamilton won?",
         answerA: "4", 
         answerB: "7", 
         answerC: "10", 
         answerD: "2",
-        answer: "7"
+        correct: "B"
     }, 
     {
         question: "Alpha Tauri is the new name of what previous team?",
@@ -74,7 +75,7 @@ var questions = [
         answerB: "Jordan", 
         answerC: "Porsche", 
         answerD: "Torro Rosso",
-        answer: "D"
+        correct: "D"
     }, 
     {
         question: "The current (2021) Formula 1 cars all use what size V6 engine (in liters)?",
@@ -82,10 +83,46 @@ var questions = [
         answerB: "2.4", 
         answerC: "3.0", 
         answerD: "3.5",
-        answer: "A"
+        correct: "A"
     }];
 
+// Variables to help with question sorting
+var currentQuestionIndex = 0;
+var lastQuestionIndex = (questions.length - 1);    
+
+
 startButton.addEventListener("click", startQuiz);
+
+
+// Starts other functions that begin the quiz
+function startQuiz(event) {
+
+    event.preventDefault();
+
+    // Calls the function to start the timer
+    countdown();
+
+    // Calls the function to shuffle the questions
+    shuffle(questions);
+
+    // Calls the function to show the first question and hides start button
+    startButton.style.display = "none";
+    showQuestion();
+    quizContainer.style.display = "block";
+}
+
+// Function that starts the timer
+function countdown() {
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timeRemaining.textContent = timeLeft;
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+            showResults();
+            return;
+        }
+    }, 1000);
+}
 
 
 // Shuffle question array each time startQuiz is called
@@ -101,59 +138,70 @@ function shuffle(questions) {
 };
 
 
-// Starts other functions that begin the quiz
-function startQuiz() {
-
-    shuffle(questions);
-
-}
-
-// Function that starts the timer
-function countdown() {
-    var timeInterval = setInterval(function () {
-        timeLeft--;
-        timeRemaining.textContent = timeLeft;
-        if (timeLeft === 0) {
-            clearInterval(timeInterval);
-        }
-    }, 1000);
-}
-
-
 // Function that shows the current question
 function showQuestion() {
     // The questionIndex should be shuffled each time the quiz is started. 
-    var currentQ = questions[questionIndex];
+    var currentQ = questions[currentQuestionIndex];
 
     // Sets the inner HTML of each container to the current question and answers
     question.innerHTML = currentQ.question;
-    answer1.innerHTML = currentQ.answerA;
-    answer2.innerHTML = currentQ.answerB;
-    answer3.innerHTML = currentQ.answerC;
-    answer4.innerHTML = currentQ.answerD;
-
-
+    answerTextA.innerHTML = currentQ.answerA;
+    answerTextB.innerHTML = currentQ.answerB;
+    answerTextC.innerHTML = currentQ.answerC;
+    answerTextD.innerHTML = currentQ.answerD;
 
 }
-
-
 
 // Function that gets next question
 // On answer click, the questionIndex will be incremented
 
+// Function to check if answer is correct
+function checkAnswer(answer) {
+    if(answer == questions[currentQuestionIndex].correct) {
+        correctAnswers++;
+        console.log(answer);
+    } else if (answer != questions[currentQuestionIndex].correct){
+        wrongAnswers++;
+        wrongAns();
+        console.log(answer);
+    }
 
-
-function correctAns(){
-
+    // Check if there are still questions remaining
+    if (currentQuestionIndex < lastQuestionIndex) {
+        currentQuestionIndex++;
+        showQuestion();
+    } else {
+        clearInterval(timeLeft);
+        showResults();
+    }
 }
 
+
+
+// function correctAns() {
+//     // change score text to show current number of correct answers
+// }
+
 function wrongAns(){
-    
+    // change score text to show current number of correct answers
+    timeLeft = timeLeft - 5;
 }
 
 
 
 // Function that will show the results at the end
 function showResults() {
+    resultsContainer.style.display = "block";
+    quizContainer.style.display = "none";
+    // Show total correct and incorrect answers
+    totalCorrect.innerHTML = "Correct: " + correctAnswers;
+    totalIncorrect.innerHTML = "Incorrect: " + wrongAnswers;
+
+    timeRemaining.textContent = '';
+}
+
+
+// Save results to local storage
+function saveResults() {
 
 }
